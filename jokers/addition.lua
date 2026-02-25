48c797caa199a2240475a51318fe4e7d58a3ae7c
rockeater500 <gkochlamazashvili555@gmail.com>
2026-02-25 23:22:04 +0400

    Add files via upload

---
+++ jokers/addition.lua
@@ -0,0 +1,70 @@
+
+SMODS.Joker{ --Addition
+    key = "addition",
+    config = {
+        extra = {
+        }
+    },
+    loc_txt = {
+        ['name'] = 'Addition',
+        ['text'] = {
+            [1] = 'Retrigger the jokers to this cards {C:blue}Left{}',
+            [2] = 'and {C:red}Right{}.'
+        },
+        ['unlock'] = {
+            [1] = 'Unlocked by default.'
+        }
+    },
+    pos = {
+        x = 0,
+        y = 0
+    },
+    display_size = {
+        w = 71 * 1, 
+        h = 95 * 1
+    },
+    cost = 8,
+    rarity = "rockeater500_math",
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+    unlocked = true,
+    discovered = true,
+    atlas = 'CustomJokers',
+    pools = { ["rockeater500_rockeater500_jokers"] = true },
+    
+    calculate = function(self, card, context)
+        
+        local target_joker = nil
+        
+        local my_pos = nil
+        for i = 1, #G.jokers.cards do
+            if G.jokers.cards[i] == card then
+                my_pos = i
+                break
+            end
+        end
+        target_joker = (my_pos and my_pos > 1) and G.jokers.cards[my_pos - 1] or nil
+        
+        local ret = SMODS.blueprint_effect(card, target_joker, context)
+        if ret then
+            SMODS.calculate_effect(ret, card)
+        end
+        
+        local target_joker = nil
+        
+        local my_pos = nil
+        for i = 1, #G.jokers.cards do
+            if G.jokers.cards[i] == card then
+                my_pos = i
+                break
+            end
+        end
+        target_joker = (my_pos and my_pos < #G.jokers.cards) and G.jokers.cards[my_pos + 1] or nil
+        
+        local ret = SMODS.blueprint_effect(card, target_joker, context)
+        if ret then
+            SMODS.calculate_effect(ret, card)
+        end
+    end
+}
---
+++ jokers/multiplication.lua
@@ -0,0 +1,51 @@
+
+SMODS.Joker{ --Multiplication
+    key = "multiplication",
+    config = {
+        extra = {
+            filledjokerslots = 0,
+            alljokerssellvalue = 1
+        }
+    },
+    loc_txt = {
+        ['name'] = 'Multiplication',
+        ['text'] = {
+            [1] = '{X:mult,C:white}X1{} Mult,',
+            [2] = 'increases by 0.1 for every {C:gold}${} in',
+            [3] = 'combined sell value of all jokers'
+        },
+        ['unlock'] = {
+            [1] = 'Unlocked by default.'
+        }
+    },
+    pos = {
+        x = 2,
+        y = 0
+    },
+    display_size = {
+        w = 71 * 1, 
+        h = 95 * 1
+    },
+    cost = 5,
+    rarity = "rockeater500_rare_math",
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+    unlocked = true,
+    discovered = true,
+    atlas = 'CustomJokers',
+    pools = { ["rockeater500_rockeater500_jokers"] = true },
+    
+    loc_vars = function(self, info_queue, card)
+        
+    return {vars = {card.ability.extra.filledjokerslots, card.ability.extra.alljokerssellvalue + ((function() local total = 0; for _, joker in ipairs(G.jokers and (G.jokers and G.jokers.cards or {}) or {}) do total = total + joker.sell_cost end; return total end)()) * 0.1}}
+    end,
+    
+    calculate = function(self, card, context)
+        if context.cardarea == G.jokers and context.joker_main  then
+            return {
+            Xmult = card.ability.extra.alljokerssellvalue + ((function() local total = 0; for _, joker in ipairs(G.jokers and G.jokers.cards or {}) do total = total + joker.sell_cost end; return total end)()) * 0.1
+            }
+        end
+    end
+}
